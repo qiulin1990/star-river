9d5a5adb305e41bd1de412a63a3cb787af41f98f
YCKJ2999 <yanggang@cloudwalk.cn>
2020-01-06 13:04:08 +0800

    second commit

---
+++ star-river-model/src/test/java/com/cloudwalk/StarRiverModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudwalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarRiverModelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
