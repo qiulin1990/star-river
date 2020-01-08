4c5a8d72f4134d735827a4fab11912ada094a21c
杨刚 <yanggang@cloudwalk.cn>
2020-01-08 17:42:49 +0800

    second commit

--- star-river-data/src/main/java/com/cloudwalk/config/WebConfig.java
+++ star-river-data/src/main/java/com/cloudwalk/config/WebConfig.java
@@ -5,6 +5,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * restemplate实例对象
+ * @author yanggang
+ * @version 1.0
+ * @since jdk 1.8
+ * @date 01-07
+ */
 @Configuration
 public class WebConfig {
     @Bean
